10d8f0f9666e0ef4c71d7634ecafdb692651c77e
luccalopes <110003708+luccalopes@users.noreply.github.com>
2023-08-22 20:15:16 -0300

    att

--- aula02/main.py
+++ aula02/main.py
@@ -1,2 +1,4 @@
 print(12, 34, sep='-', end='\r\n')
 print(56, 78, sep='-', end='\r\n')
+
+print('Explicito', 'é', 'melhor " do que implicito')
---
+++ aula04/main.py
@@ -0,0 +1,4 @@
+print(11.5 + 22.8)
+print(type('Lucca'))
+print(type(12))
+print(type(1.2), type(-1.1), type(0.0))
