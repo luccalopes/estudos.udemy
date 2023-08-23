48e6236a9bdfa829d088c5994991bce85f81bd03
luccalopes <110003708+luccalopes@users.noreply.github.com>
2023-08-23 15:43:29 -0300

    23/08

--- aula04/main.py
+++ aula04/main.py
@@ -2,3 +2,16 @@ print(11.5 + 22.8)
 print(type('Lucca'))
 print(type(12))
 print(type(1.2), type(-1.1), type(0.0))
+print(10 == '10')
+print(int(1), type(int('1')))
+print(float('1') + 1)
+
+def concatenateString() :
+    print('10' + '10')
+
+def callMyName(myName) :
+    full_name = myName
+    print('Hello,', full_name)
+
+concatenateString()
+callMyName('Lucca C. Lopes')
