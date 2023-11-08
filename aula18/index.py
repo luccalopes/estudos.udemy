d85a1ae196da9e1421972056efca5cc244a7d599
luccalopes <110003708+luccalopes@users.noreply.github.com>
2023-11-08 08:42:41 -0300

    zzz

---
+++ aula18/index.py
@@ -0,0 +1,23 @@
+# if, elif e else
+
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('código para condição 1')
+    print('código para condição 1')
+elif condicao2:
+    print('código para condição 2')
+elif condicao3:
+    print('código para condição 3')
+elif condicao4:
+    print('código para condição 4')
+else:
+    print('nenhuma condição é válida')
+
+if 10 == 10:
+    print('qualquer coisa')
+
+print('fora do if')
