b3a69e1bee6b7653a40a547a48ee73d0468e9abf
luccalopes <110003708+luccalopes@users.noreply.github.com>
2023-12-11 08:24:39 -0300

    z

--- aula26/index.py
+++ aula26/index.py
@@ -3,6 +3,10 @@ print(f'{variavel}')
 print(f'{variavel: >10}')
 print(f'{variavel: <10}')
 print(f'{variavel: ^10}')
-print(f'{-1000.378378346:+,.2f}')
+print(f'{+1000.378378346:+,.2f}')
 
-# 
+# > significa que vai ter 10 espaços vazios à direita da string da variavel
+# < significa que vai ter 10 espaços vazios à esquerda da string
+# ^ significa que estamos pedindo para encaixar a string da variável no meio de 10 espaços vazios
+# para indicar se um número int ou float é positivo ou negativo, utilizar os sinais + ou - após abrir as chaves
+# das f'strings, antes do número e após o número, inserir : e depois o sinal
