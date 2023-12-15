e7d19d549cbb4ca378ef7b1167be41561aee7796
luccalopes <110003708+luccalopes@users.noreply.github.com>
2023-12-15 08:19:25 -0300

    zz

--- aula29/index.py
+++ aula29/index.py
@@ -1,3 +1,21 @@
 numero_str = input('vou dobrar o número que vc digitar ')
-numero_int = int(numero_str)
-print(f'o dobro de {numero_int} é {numero_int * 2}')
+
+try:
+    print('STR ', numero_str)
+    numero_float = float(numero_str)
+    print('FLOAT: ', numero_str)
+    print(f'O dobro de {numero_str} é {numero_float * 2}')
+except:
+    print('Isso não é um número')
+
+# numero_int = int(numero_str)
+
+#if numero_str.isdigit(): 
+#    numero_float = float(numero_str)
+#    print(f'o dobro de {numero_str} é {numero_float * 2}')
+#else:
+#    print(f'isso não é um número')
+
+
+# o try/ except funcionam de forma semelhante ao if/else
+# quando declaramos um bloco de código dentro do try, o python vai tentar executar este bloco se não houver nada que cause um erro no código, se houver algo errado, ele já cai direto no except
---
+++ aula30/index.py
@@ -0,0 +1,9 @@
+velocidade = 60
+local_carro = 90
+
+RADAR_1 = 60
+LOCAL_1 = 100
+RADAR_RANGE = 1
+
+if velocidade > RADAR_1:
+    print('Você está acima do limite de velocidade')
